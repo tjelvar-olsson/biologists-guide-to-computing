4e4a00447b0376ab124bb917f1e1494561c514b9
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2016-01-04 13:43:34 +0000

    Updated latex configuration to show urls and pagerefs.

--- source/conf.py
+++ source/conf.py
@@ -243,10 +243,10 @@ latex_logo = "images/cover-image.png"
 #latex_use_parts = False
 
 # If true, show page references after internal links.
-#latex_show_pagerefs = False
+latex_show_pagerefs = True
 
 # If true, show URL addresses after external links.
-#latex_show_urls = False
+latex_show_urls = True
 
 # Documents to append as an appendix to all manuals.
 #latex_appendices = []
