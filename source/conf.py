8b5155814edd29e06c8585a636fd4cc1ae216fc3
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2016-08-04 18:18:08 +0100

    Updated version to 0.9.1

--- source/conf.py
+++ source/conf.py
@@ -58,7 +58,7 @@ author = u'Tjelvar S. G. Olsson'
 # built documents.
 #
 # The short X.Y version.
-version = '0.9.0'
+version = '0.9.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
